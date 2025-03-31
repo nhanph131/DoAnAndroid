db31ad81c65b27e69049bab039c93e49df4ee9ae
Nhan Pham <huunhan1301200412763112@gmail.com>
2025-03-31 15:28:57 +0700

    tao home activity -activity chinh

--- app/src/main/java/com/nhanph/doanandroid/HomeActivity.java
+++ app/src/main/java/com/nhanph/doanandroid/HomeActivity.java
@@ -4,11 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignUpActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
     }
--- app/src/main/java/com/nhanph/doanandroid/LoginActivity.java
+++ app/src/main/java/com/nhanph/doanandroid/LoginActivity.java
@@ -2,14 +2,12 @@ package com.nhanph.doanandroid;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
     }
